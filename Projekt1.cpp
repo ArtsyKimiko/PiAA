79f00cbeb8a0984cbcbef56853267ef4c921f8f0
ArtsyKimiko <114069062+ArtsyKimiko@users.noreply.github.com>
2024-03-29 17:35:21 +0000

    update

--- Projekt1.cpp
+++ Projekt1.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <chrono>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 void insert(int tab[], int i)
@@ -9,82 +12,33 @@ void insert(int tab[], int i)
         insert(tab,(i-1)/2);
     }
 }
-void ins2(int tab[], int i, int size)
-{
-    if((2*i+1)<size && tab[2*i+1]>tab[i])
-    {
-        swap(tab[2*i+1], tab[i]);
-        ins2(tab,2*i+1,size);
-    }
-    if((2*i+2)<size && tab[2*i+2]>tab[i])
-    {
-        swap(tab[2*i+2], tab[i]);
-        ins2(tab,2*i+2,size);
-    }
-}
-
 void sort(int tab[], int i, int size)
 {
-    if((2*i+2)<size && (2*i+1)<size && tab[2*i+1]<tab[2*i+2])
-    {
-        swap(tab[2*i+1],tab[2*i+2]);
-        ins2(tab,(2*i+2),size);
-
-        sort(tab,(2*i+1),size);
-    }
+    swap(tab[0],tab[i]);
+    for(int j=0;j<i;j++)
+        insert(tab,j);
 }
-
 int main()
 {
-    int size = 15;
+    int size = 50000;
     int tab[size];
-    
+
+    srand(time(NULL));
     for(int i=0;i<(size+1);i++)
-        tab[i]=i;
+        tab[i]=rand()%1000;
 
-    for(int i=0;i<size;i++)
-    {
-        int s=0;
-        if(i==0) s=(size/2);
-        if(i>0 && i<3) s=(size/3);
-        if(i>2 && i<7) s=(size/6);
-        for(int j=0;j<s;j++)
-            cout<<" ";
+    auto start = chrono::high_resolution_clock::now();
 
-        cout<<tab[i]<<" ";
-        
-        if(i==0 || i==2 || i==6)
-            cout<<endl;
-    }
-    
     for(int i=0;i<size;i++)
         insert(tab,i);
 
-    cout<<endl<<endl;
-    for(int i=0;i<size;i++)
-    {
-        int s=0;
-        if(i==0) s=(size/2);
-        if(i>0 && i<3) s=(size/3);
-        if(i>2 && i<7) s=(size/6);
-        for(int j=0;j<s;j++)
-                cout<<" ";
-        cout<<tab[i]<<" ";
-        
-        if(i==0 || i==2 || i==6)
-            cout<<endl;
-    }
-
-    for(int i=0;i<size;i++)
+    for(int i=size;i>0;i--)
         sort(tab,i,size);
 
-    cout<<endl<<endl;
-    for(int i=0;i<size;i++)
-        cout<<tab[i]<<" ";
-    
-        
-        
-    cout<<endl;
+    auto stop = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
+
+    cout << size << "\t" << duration.count() << endl;
 
     return 0;
 }
